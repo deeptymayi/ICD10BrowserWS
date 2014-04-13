9a1902d3c565d5d1aa2261c546b66bda02af32f0
pawasthi <pratibhaawasthi@gmail.com>
2014-04-13 02:36:16 -0700

    weservice integration for billing fraud summary pie chart

--- WebContent/js/billingFraudDetection.js
+++ WebContent/js/billingFraudDetection.js
@@ -1,50 +1,8 @@
 $(function () {
-
-    $('#tabs').tab();  
-    
-	$( "#fromdate" ).calendarsPicker({yearRange: 'any'});
-	
+    var summaryDetails;
+    $('#tabs').tab();      
+	$( "#fromdate" ).calendarsPicker({yearRange: 'any'});	
 	$( "#todate" ).calendarsPicker({yearRange: 'any'});
-	
-    $('#container2').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
-        },
-        title: {
-            text: 'Billing Fraud Summary'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    color: '#000000',
-                    connectorColor: '#000000',
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-                }
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Browser share',
-            data: [
-                ['Correct Billing',   35.5],
-                {
-                    name: 'Billing Fraud',
-                    y: 54,
-                    sliced: true,
-                    selected: true
-                },
-                ['Bill not generated',    9.5]
-            ]
-        }]
-    });    
 	
     var billingFraudDetection= function() {
 		var fromDate;
@@ -85,6 +43,61 @@ $(function () {
 				alert("error");
 			}
 		});		
+		
+		$.ajax({
+			type: "GET",
+			url: "/ICD10BrowserWS/rest/bfr/fetchSummaryForRange/" + fromDate + "_" + toDate,
+			headers: {"Accept": "application/json", "Content-Type" : "application/json"},
+			dataType: 'json',
+			success: function(data) {
+				alert("hi");
+				summaryDetails = JSON.parse(data);
+			},
+			error: function(xhr, textStatus, errorThrown) {
+				alert("error");
+			}
+		}).done(function(){	
+		
+			$('#container2').highcharts({
+		        chart: {
+		            plotBackgroundColor: null,
+		            plotBorderWidth: null,
+		            plotShadow: false
+		        },
+		        title: {
+		            text: 'Billing Fraud Summary'
+		        },
+		        tooltip: {
+		            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+		        },
+		        plotOptions: {
+		            pie: {
+		                allowPointSelect: true,
+		                cursor: 'pointer',
+		                dataLabels: {
+		                    enabled: true,
+		                    color: '#000000',
+		                    connectorColor: '#000000',
+		                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+		                }
+		            }
+		        },
+		        series: [{
+		            type: 'pie',
+		            name: 'Percentage',
+		            data: [
+		                ['Correct Billing',  (typeof summaryDetails.correctBills != "undefined") ? (summaryDetails.correctBills/summaryDetails.totalBills) : 0 ],//35.5],
+		                {
+		                    name: 'Billing Fraud',
+		                    y: (typeof summaryDetails.fraudBills != "undefined")  ? (summaryDetails.fraudBills/summaryDetails.totalBills) : 0 , //54,
+		                    sliced: true,
+		                    selected: true
+		                },
+		                ['Bill not generated',   (typeof summaryDetails.billNotGenerated != "undefined")  ? (summaryDetails.billNotGenerated/summaryDetails.totalBills) : 0 ] // 9.5]
+		            ]
+		        }]
+		    });    
+		});
 	};
 	
 	$( "#searchBillingFraud" ).click(billingFraudDetection);
