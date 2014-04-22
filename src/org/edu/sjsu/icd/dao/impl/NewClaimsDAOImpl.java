0d5af7cee531ddeae5eb4b1241b25a29ce300fd9
Deeptymayi <deeptymayi.patel@gmail.com>
2014-04-22 00:46:57 -0700

    Added the cron for kicking the fraud detection code.

--- src/org/edu/sjsu/icd/dao/impl/NewClaimsDAOImpl.java
+++ src/org/edu/sjsu/icd/dao/impl/NewClaimsDAOImpl.java
@@ -1,5 +1,9 @@
 package org.edu.sjsu.icd.dao.impl;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import javax.sql.DataSource;
 
 import org.edu.sjsu.icd.dao.INewClaimsDAO;
@@ -26,12 +30,30 @@ public class NewClaimsDAOImpl implements INewClaimsDAO {
 	@Override
 	public void refresh() {
 
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd 00:00:00");
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -1);
+		String date = dateFormat.format(cal.getTime());
+
 		// Query to insert Patient information to the Patient DB Table
 		String query1 = "TRUNCATE new_claims";
-		String query2 = "INSERT INTO NEW_CLAIMS (patient_Id, first_name, last_name, bill_number, diagnosis, billed_icd_code, billing_date, master_icd_code, icd_description) SELECT p.patient_Id, p.first_name, p.last_name, mr.bill_number, mr.diagnosis, mr.icd10_code, mb.billing_date, icd.icd_code, icd.description FROM patient p LEFT JOIN medical_record mr ON mr.patient_id = p.patient_id LEFT JOIN medical_bill mb ON mb.bill_number = mr.bill_number LEFT JOIN icd_code_to_disease_mapping icd ON mb.icd10_code = icd.icd_code WHERE mr.bill_number IS NOT NULL AND mb.billing_date IS NOT NULL AND mr.diagnosis is NOT NULL AND mr.icd10_code is NOT NULL";
+		String query2 = "INSERT INTO NEW_CLAIMS (patient_Id, first_name, last_name, bill_number, "
+		        + "diagnosis, billed_icd_code, billing_date, master_icd_code, icd_description) "
+		        + "SELECT p.patient_Id, p.first_name, p.last_name, mr.bill_number, mr.diagnosis, "
+		        + "mr.icd10_code, mb.billing_date, icd.icd_code, icd.description FROM patient p "
+		        + "LEFT JOIN medical_record mr ON mr.patient_id = p.patient_id LEFT JOIN medical_bill mb "
+		        + "ON mb.bill_number = mr.bill_number LEFT JOIN icd_code_to_disease_mapping icd "
+		        + "ON mb.icd10_code = icd.icd_code WHERE mr.bill_number IS NOT NULL AND mb.billing_date IS NOT NULL "
+		        + "AND mr.diagnosis is NOT NULL AND mr.icd10_code is NOT NULL AND billing_date > '" + date
+		        + "'";
 
 		// Create a query using the JDBC template and insert the record.
 		jdbcTemplate.execute(query1);
+
+		System.out.println("Trucated the NEW_CLAIMS table.");
+
 		jdbcTemplate.execute(query2);
+
+		System.out.println("Populated the NEW_CLAIMS table with new claims entered yesterday (" + date + ")");
 	}
 }
--- src/org/edu/sjsu/icd/service/FraudViewerService.java
+++ src/org/edu/sjsu/icd/service/FraudViewerService.java
@@ -8,14 +8,28 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.db.DBConfiguration;
+import org.apache.hadoop.mapreduce.lib.db.DBInputFormat;
+import org.apache.hadoop.mapreduce.lib.db.DBOutputFormat;
 import org.edu.sjsu.icd.dao.IDiseaseDAO;
 import org.edu.sjsu.icd.dao.IFraudBillsDAO;
 import org.edu.sjsu.icd.dao.IMedBillDAO;
 import org.edu.sjsu.icd.dao.INewClaimsDAO;
+import org.edu.sjsu.icd.fdmr.FDMapper;
+import org.edu.sjsu.icd.fdmr.FDReducer;
+import org.edu.sjsu.icd.fdmr.FraudDetectionMRDriver;
+import org.edu.sjsu.icd.fdmr.InputWritable;
+import org.edu.sjsu.icd.fdmr.OutputWritable;
 import org.edu.sjsu.icd.utils.ObjectJsonBidirectionalConverter;
 import org.edu.sjsu.icd.vo.Bill;
 import org.edu.sjsu.icd.vo.Bills;
 import org.edu.sjsu.icd.vo.Disease;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * @author Deepti
@@ -138,10 +152,48 @@ public class FraudViewerService {
 	}
 
 	/**
-	 * 
+	 * This cron job will identify the new claim data that will be analyzed for
+	 * fraud.
 	 */
+	@Scheduled(cron = "0 01 00 * * *")
 	public void prepareNewClaimsForFraudDetection() {
 		System.out.println("Firing the cron for fraud detection.");
 		newClaimsDAO.refresh();
+	}
+
+	/**
+	 * This cron job will trigger the mapreduce for fraud analysis of the new
+	 * claims data.
+	 */
+	@Scheduled(cron = "0 11 00 * * *")
+	public void runFraudDetectionMRJob() {
+		System.out.println("Firing the cron for MR detection job.");
+		try {
+			Configuration conf = new Configuration();
+			DBConfiguration.configureDB(conf, "com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/ICD10",
+			        "root", "destiny");
+
+			Job job = new Job(conf);
+			job.setJarByClass(FraudDetectionMRDriver.class);
+			job.setMapperClass(FDMapper.class);
+			job.setReducerClass(FDReducer.class);
+			job.setMapOutputKeyClass(Text.class);
+			job.setMapOutputValueClass(IntWritable.class);
+			job.setOutputKeyClass(OutputWritable.class);
+			job.setOutputValueClass(NullWritable.class);
+			job.setInputFormatClass(DBInputFormat.class);
+			job.setOutputFormatClass(DBOutputFormat.class);
+
+			DBInputFormat.setInput(job, InputWritable.class, "new_claims", null, null, new String[] {
+			        "patient_id", "first_name", "last_name", "bill_number", "diagnosis", "billed_icd_code",
+			        "billing_date", "master_icd_code", "icd_description" });
+			DBOutputFormat.setOutput(job, "fraud_bills", new String[] { "bill_number", "first_name",
+			        "last_name", "billing_date", "symptoms", "bill_icd" });
+
+			job.waitForCompletion(true);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
